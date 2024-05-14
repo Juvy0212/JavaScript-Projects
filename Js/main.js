f8a570674f8cd68c3b2c8ba13ce152442cea9c59
Juvy0212 <Toralbajuvylyn25@gmail.com>
2024-05-14 18:01:36 +0800

    Add Project1_expression_alert

---
+++ Js/main.js
@@ -0,0 +1,22 @@
+var x='Hi Juvylyn!';
+
+
+window.alert(x);
+
+var c = "\"Hi says Mark\" \"hello\" says Juvy " + "They talked to each other<br><br><br>";
+
+document.write(c);
+
+var family="Leron", Dad = "Mark", Daughter ="Maxine", Son="Matthew";
+
+document.write(2+2);
+
+function my_first_function(){
+    var str="This is the button text!";
+    document.getElementById("Button_text").innerHTML = str; //after clicking the button in webpage it will display the the function
+}
+
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string";
+
+document.write(Sent1 + Sent2); //will display the value of the string
+
