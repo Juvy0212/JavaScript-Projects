25b5354ea604d41a2bfeeeea5ea8a596e35a686d
Juvy0212 <Toralbajuvylyn25@gmail.com>
2024-05-23 10:07:26 +0800

    Initialized

---
+++ js/one_page_website.js
@@ -0,0 +1,32 @@
+// JavaScript code to handle the modal functionality
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+    captionText.innerHTML = slides[slideIndex-1].getElementsByTagName("img")[0].alt;
+}
